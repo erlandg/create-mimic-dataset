2976c209e9427d2a5f6de36884f4ed508865075b
Erland Grimstad <erland.grimstad@yahoo.no>
2022-06-14 14:58:31 +0200

    added credentials

--- download.sh
+++ download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+source credentials
 input="$1"
 echo "Reading file: $1"
 while IFS= read -r line
 do
-  wget -r -N -c -np --user INSERT_USERNAME_HERE --password INSERT_PASSWORD_HERE https://physionet.org/files/mimic-cxr/2.0.0/"$line"
+  wget -r -N -c -np --user $USER --password $PASS https://physionet.org/files/mimic-cxr/2.0.0/"$line"
 done < "$input"
